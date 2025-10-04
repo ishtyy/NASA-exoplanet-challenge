4239fb01d4698837059f25067b275d3a1df788b6
ishtyy <2205057@ugrad.cse.buet.ac.bd>
2025-10-04 13:43:51 +0600

    Deployment setup fix

--- webapp/app.py
+++ webapp/app.py
@@ -147,5 +147,8 @@ def server_error(e):
     return jsonify({"error": "Internal server error"}), 500
 
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
